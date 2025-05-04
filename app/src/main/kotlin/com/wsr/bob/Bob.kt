71559139bc24d973a670578694820254854c6497
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2025-05-04 15:31:47 +0900

    [add] Bobを追加

---
+++ app/src/main/kotlin/com/wsr/bob/Bob.kt
@@ -0,0 +1,23 @@
+package com.wsr.bob
+
+import com.wsr.external.Human
+import com.wsr.external.Topic
+
+class Bob : Human {
+    val values: MutableMap<Topic, String> = mutableMapOf(
+        Topic.Hobby to "登山",
+    )
+
+    override fun response(topic: Topic): String = values.getOrPut(topic) { "分からない" }
+
+    fun input(human: Human) {
+        values
+            .forEach { (topic, myValues) ->
+                val externalValues = human.response(topic)
+                val newValues = compare(myValues, externalValues)
+                values[topic] = newValues
+            }
+    }
+
+    private fun compare(current: String, new: String): String = listOf(current, new).random()
+}
